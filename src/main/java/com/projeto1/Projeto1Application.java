06011677c9a94a410c65e0d9b4ae2976d49b0ef9
Jonatas <jonatas.luis@gmail.com>
2019-03-26 08:41:03 -0300

    instanciando duas categorias

--- src/main/java/com/projeto1/Projeto1Application.java
+++ src/main/java/com/projeto1/Projeto1Application.java
@@ -1,13 +1,33 @@
 package com.projeto1;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.projeto1.domain.Categoria;
+import com.projeto1.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class Projeto1Application {
+public class Projeto1Application  implements CommandLineRunner {
+	
+	@Autowired
+	CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Projeto1Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		
 	}
 
 }
