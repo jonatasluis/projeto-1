a5a93ed6013ffa8afbfafbb984092fd1a8d2292d
Jonatas <jonatas.luis@gmail.com>
2019-03-26 13:57:45 -0300

    ajustes no endpoint /categorias/{id}

---
+++ src/main/java/com/projeto1/Resources/exceptions/StandardError.java
@@ -0,0 +1,45 @@
+package com.projeto1.resources.exceptions;
+
+import java.io.Serializable;
+
+public class StandardError implements Serializable{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private Integer status;
+	private String msg;
+	private long timeStamp;
+	
+	
+	
+	public StandardError(Integer status, String msg, long timeStamp) {
+		super();
+		this.status = status;
+		this.msg = msg;
+		this.timeStamp = timeStamp;
+	}
+	
+	public Integer getStatus() {
+		return status;
+	}
+	public String getMsg() {
+		return msg;
+	}
+	public long getTimeStamp() {
+		return timeStamp;
+	}
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+	public void setTimeStamp(long timeStamp) {
+		this.timeStamp = timeStamp;
+	}
+	
+	
+
+}
--- src/main/java/com/projeto1/services/CategoriaService.java
+++ src/main/java/com/projeto1/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.projeto1.domain.Categoria;
 import com.projeto1.repositories.CategoriaRepository;
+import com.projeto1.services.exceptios.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -14,9 +15,12 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 	
+
+	
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
-	}
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+		}
 
 }
