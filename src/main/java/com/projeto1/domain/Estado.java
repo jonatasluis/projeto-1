82059443c455640a8764cc1e45339ac5e2983718
Jonatas <jonatas.luis@gmail.com>
2019-03-29 10:50:36 -0300

    endpoint e referencia ciclica cliente

--- src/main/java/com/projeto1/domain/Estado.java
+++ src/main/java/com/projeto1/domain/Estado.java
@@ -10,6 +10,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 @Entity
 public class Estado implements Serializable{
 	
@@ -27,7 +29,7 @@ public class Estado implements Serializable{
 	@OneToMany(mappedBy="estado")
 	private List<Cidade> cidades = new ArrayList<>();
 	
-
+	@JsonBackReference
 	public List<Cidade> getCidades() {
 		return cidades;
 	}
