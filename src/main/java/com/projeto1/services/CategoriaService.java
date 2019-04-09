bfa0f637d1d5a4f7b126d5f8d364747266290092
Jonatas <jonatas.luis@gmail.com>
2019-04-09 10:49:06 -0300

    Correções em update de Categoria e construtor de Pagamento

--- src/main/java/com/projeto1/services/CategoriaService.java
+++ src/main/java/com/projeto1/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.projeto1.domain.Categoria;
+
 import com.projeto1.dto.CategoriaDTO;
 import com.projeto1.repositories.CategoriaRepository;
 import com.projeto1.services.exceptios.DataIntegrityException;
@@ -41,8 +42,10 @@ public class CategoriaService {
 
 
 	public Categoria update(Categoria obj) {
-		find(obj.getId());
-		return repo.save(obj); 
+		
+		Categoria newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj); 
 	}
 
 
@@ -75,6 +78,9 @@ public class CategoriaService {
 	}
 
 
-
+	private void updateData(Categoria newObj, Categoria obj) {
+		newObj.setNome(obj.getNome());
+		
+	}
 
 }
